5200376d0f94e425c38835fe782bf6bf5af00ef7
Harririn <98252093+Harririn@users.noreply.github.com>
2022-01-25 12:46:54 -0800

    Minor changes to default and forest theme

--- Harry-FactFinder/script.js
+++ Harry-FactFinder/script.js
@@ -5,35 +5,35 @@ document.querySelector('#menu').d.addEventListener("click",showD);
 document.querySelector('#menu').e.addEventListener("click",showE);
 
 function showA(){
-	document.getElementById('words').innerHTML = "An Octopus has three hearts <img src='images/octopus.jpg' alt='Photo of an octopus' id='image'>";
+	document.getElementById('words').innerHTML = "An Octopus has three hearts. <img src='images/octopus.jpg' alt='Photo of an octopus' id='image'>";
 }
 function showB(){
-	document.getElementById('words').innerHTML = "The different colors of Froot Loops cereal all taste the same <img src='images/cereal.jpg' alt='Photo of a bowl of cereal' id='image'>";
+	document.getElementById('words').innerHTML = "The different colors of Froot Loops cereal all taste the same. <img src='images/cereal.jpg' alt='Photo of a bowl of cereal' id='image'>";
 }
 function showC(){
-	document.getElementById('words').innerHTML = "On May 22, 2010 Laszlo Hanyecz, purchased two Papa Johns pizza using 10,000 bitcoins <img src='images/pizza.jpg' alt='Photo of a pizza' id='image'>";
+	document.getElementById('words').innerHTML = "On May 22, 2010 Laszlo Hanyecz, purchased two Papa Johns pizza using 10,000 bitcoins. <img src='images/pizza.jpg' alt='Photo of a pizza' id='image'>";
 }
 function showD(){
-	document.getElementById('words').innerHTML = "The world's driest desert, the Atacama Desert, is right next to the ocean <img src='images/desert.jpg' alt='Photo of the Atacama desert' id='image'>";
+	document.getElementById('words').innerHTML = "The world's driest desert, the Atacama Desert, is right next to the ocean. <img src='images/desert.jpg' alt='Photo of the Atacama desert' id='image'>";
 }
 function showE(){
-	document.getElementById('words').innerHTML = "The inventor of cotton candy, WIlliam Morrison, was a dentist <img src='images/cottoncandy.jpg' alt='Photo of cotton candy' id='image'>";
+	document.getElementById('words').innerHTML = "The inventor of cotton candy, WIlliam Morrison, was a dentist. <img src='images/cottoncandy.jpg' alt='Photo of cotton candy' id='image'>";
 }
 
 document.querySelector('#theme').x.addEventListener("click",showDefault);
 
 function showDefault(){
 	document.querySelector('body').style.backgroundColor = "#7CC6FE";
-	document.querySelector('section').style.backgroundColor = "#D3FAD6";
+	document.querySelector('section').style.backgroundColor = "#D1EFB5";
 	document.querySelector('section').style.color = "#FBB8B1";
 	document.querySelector('section').style.borderColor = "#EDEBA0";
-	document.querySelector('section').style.fontFamily = "Helvetica";
+	document.querySelector('section').style.fontFamily = "Courier New";
 	var inputs = document.getElementsByTagName('input');
 	for(var i = 0; i < inputs.length; i++){
 		inputs[i].style.color = "#FBB8B0";
 		inputs[i].style.backgroundColor = "#D1EFB5";
 		inputs[i].style.borderColor = "#EDEBA0";
-		inputs[i].style.fontFamily = "Helvetica";
+		inputs[i].style.fontFamily = "Courier New";
 	}
 }
 
@@ -43,13 +43,13 @@ function showForest(){
 	document.querySelector('body').style.backgroundColor = "#CADF9E";
 	document.querySelector('section').style.backgroundColor = "#A4AC96";
 	document.querySelector('section').style.color = "#313628";
-	document.querySelector('section').style.borderColor = "#a4ac96";
+	document.querySelector('section').style.borderColor = "#746D72";
 	document.querySelector('section').style.fontFamily = "Papyrus";
 	var inputs = document.getElementsByTagName('input');
 	for(var i = 0; i < inputs.length; i++){
 		inputs[i].style.color = "#313628";
 		inputs[i].style.backgroundColor = "#A4AC96";
-		inputs[i].style.borderColor = "#a4ac96";
+		inputs[i].style.borderColor = "#746D72";
 		inputs[i].style.fontFamily = "Papyrus";
 	}
 }
